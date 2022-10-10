c3985fb0053231428ba8beadda181c09deae462a
Angel Acuña <acuhangel@gmail.com>
2022-10-09 23:51:43 -0500

    oneMaxV3

--- lib/genetic.ex
+++ lib/genetic.ex
@@ -2,10 +2,13 @@ defmodule Genetic do
   @moduledoc """
   Documentation for `Genetic`.
   """
-  def run(fitness_function, genotype, max_fitness, opts \\ []) do
-    population = initialize(genotype)
+  alias Types.Chromosome
+
+  def run(problem, opts \\ []) do
+    population = initialize(&problem.genotype/0)
+
     population
-    |> evolve(fitness_function, genotype, max_fitness, opts)
+    |> evolve(problem, opts)
   end
 
   def initialize(genotype, opts \\ []) do
@@ -15,7 +18,12 @@ defmodule Genetic do
 
   def evaluate(population, fitness_function, opts \\ []) do
     population
-    |> Enum.sort_by(fitness_function, &>/2)
+    |> Enum.map(fn chromosome ->
+      fitness = fitness_function.(chromosome)
+      age = chromosome.age + 1
+      %Chromosome{chromosome | fitness: fitness, age: age}
+    end)
+    |> Enum.sort_by(& &1.fitness, &>/2)
   end
 
   def select(population, opts \\ []) do
@@ -26,13 +34,17 @@ defmodule Genetic do
 
   def crossover(population, opts \\ []) do
     population
-    |> Enum.reduce([],
+    |> Enum.reduce(
+      [],
       fn {p1, p2}, acc ->
-        cx_point = :rand.uniform(length(p1))
-        {{h1, t1}, {h2, t2}} =
-          {Enum.split(p1, cx_point),
-          Enum.split(p2, cx_point)}
-        {c1, c2} = {h1 ++ t2, h2 ++ t1}
+        cx_point = :rand.uniform(length(p1.genes))
+        {{h1, t1}, {h2, t2}} = {Enum.split(p1.genes, cx_point), Enum.split(p2.genes, cx_point)}
+
+        {c1, c2} = {
+          %Chromosome{p1 | genes: h1 ++ t2},
+          %Chromosome{p2 | genes: h2 ++ t1}
+        }
+
         [c1, c2 | acc]
       end
     )
@@ -40,29 +52,28 @@ defmodule Genetic do
 
   def mutation(population, opts \\ []) do
     population
-    |> Enum.map(
-      fn chromosome ->
-        if :rand.uniform() < 0.05 do
-          Enum.shuffle(chromosome)
-        else
-          chromosome
-        end
+    |> Enum.map(fn chromosome ->
+      if :rand.uniform() < 0.05 do
+        %Chromosome{chromosome | genes: Enum.shuffle(chromosome.genes)}
+      else
+        chromosome
       end
-    )
+    end)
   end
 
-  def evolve(population, fitness_function,genotype, max_fitness, opts \\ []) do
-    population = evaluate(population, fitness_function, opts )
+  def evolve(population, problem, opts \\ []) do
+    population = evaluate(population, &problem.fitness_function/1, opts)
     best = hd(population)
-    IO.write("\ncurrent best: #{fitness_function.(best)}")
-    if fitness_function.(best) == max_fitness do
+    IO.write("\ncurrent best: #{best.fitness}")
+
+    if problem.terminate?(population) do
       best
     else
       population
       |> select(opts)
       |> crossover(opts)
       |> mutation(opts)
-      |> evolve(fitness_function, genotype, max_fitness, opts)
+      |> evolve(problem, opts)
     end
   end
 end
--- scripts/one_max.exs
+++ scripts/one_max.exs
@@ -1,7 +1,31 @@
-genotype = fn -> for _ <- 1..1000, do: Enum.random(0..1) end
-max_fitnes = 1000
-fitness_function = fn chromosome -> Enum.sum (chromosome) end
-slon = Genetic.run(fitness_function, genotype, max_fitnes, [])
+# 3rd version
+defmodule OneMax do
+  @behaviour Problem
 
+  alias Types.Chromosome
+
+  @impl true
+  def genotype do
+    genes = for _ <- 1..42, do: Enum.random(0..1)
+    %Chromosome{genes: genes, size: 42}
+  end
+
+  @impl true
+  def fitness_function(chromosome), do: Enum.sum(chromosome.genes)
+
+  @impl true
+  def terminate?([best | _]), do: best.fitness == 42
+end
+
+soln = Genetic.run(OneMax)
 IO.write("\n")
-IO.inspect(slon)
+IO.inspect(soln)
+
+# 2nd version
+# genotype = fn -> for _ <- 1..1000, do: Enum.random(0..1) end
+# max_fitnes = 1000
+# fitness_function = fn chromosome -> Enum.sum (chromosome) end
+# slon = Genetic.run(fitness_function, genotype, max_fitnes, [])
+
+# IO.write("\n")
+# IO.inspect(slon)
